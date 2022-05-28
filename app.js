b9d6f4f503fef20ad9b652d872d38123d98ff0ef
Junior Schmidt <junior.schmidt.99@gmail.com>
2022-05-28 02:23:09 -0300

    Writer

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const Reader = require('./Reader');
 const Processor = require('./Processor');
 const Table = require('./Table')
 const HtmlParser = require('./HtmlParser')
-const leitor = new Reader();
+const Writer = require('./Writer');
 
-csvfilepath = './users.csv'         // CSV File
+const reader = new Reader();
+const writer = new Writer();
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+
+csvfile = './users.csv'       // CSV File
+htmlfile = 'html.html'        // SAVE html File
 
 async function main() {
-    var rawdata = await leitor.Read(csvfilepath)
+    var rawdata = await reader.Read(csvfile)
     processeddata = Processor.Process(rawdata);
-
     var users = new Table(processeddata);
-
-    console.log(users)
     var html = await HtmlParser.Parse(users);
-
-    console.log(html);
+    writer.Write(today.toDateString()+'.html', html);
 }
 
 main();
